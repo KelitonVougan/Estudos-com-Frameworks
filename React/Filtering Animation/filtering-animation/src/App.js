8f1901e701534541ed7b0125ce00cc76e7b761c4
KelitonVougan <kelitonvougan@gmail.com>
2022-11-18 00:42:35 -0300

    modified:   React/Filtering Animation/filtering-animation/src/App.css 	modified:   React/Filtering Animation/filtering-animation/src/App.js 	new file:   React/Filtering Animation/filtering-animation/src/Movie.js

--- React/Filtering Animation/filtering-animation/src/App.js
+++ React/Filtering Animation/filtering-animation/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import {useEffect, useState} from 'react';
+import Movie from './Movie';
+import Filter from './Filter';
 
 function App() {
+
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopularMovies();
+  },[]);
+
+const fetchPopularMovies = async () => {
+  const dataFromApi = await fetch ('https://api.themoviedb.org/3/movie/popular?api_key=e03354548d1fec9ca11d314ea7a6129d&language=en-US&page=1');
+  const movies = await dataFromApi.json();
+  setPopular(movies.results);
+  setFiltered(movies.results);
+
+}
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="popular-movies">
+        <Filter popular={popular} setFiltered={setFiltered}/>
+        {popular.map((movie) => {
+          return <Movie key={movie.id} movie={movie}/>;
+        })}
+      </div>
     </div>
   );
 }
