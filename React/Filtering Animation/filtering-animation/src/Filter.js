9cad836b6b604e936f3a741f6ffded4cf1addfc8
KelitonVougan <kelitonvougan@gmail.com>
2022-11-18 00:42:16 -0300

    new file:   React/Filtering Animation/filtering-animation/src/Filter.js 	modified:   React/Filtering Animation/filtering-animation/src/index.css 	modified:   React/Filtering Animation/filtering-animation/src/App.css 	modified:   React/Filtering Animation/filtering-animation/src/App.js

---
+++ React/Filtering Animation/filtering-animation/src/Filter.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+function Filter(){
+    return(
+        <div className="filter-container">
+            <button>Todos</button>
+            <button>Comedia</button>
+            <button>Ação</button>
+        </div>
+    )
+}
+
+export default Filter;
