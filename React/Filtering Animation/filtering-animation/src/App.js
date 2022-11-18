29e504b617c905d5372a71e9282e94d07cc44ee2
KelitonVougan <kelitonvougan@gmail.com>
2022-11-18 02:17:56 -0300

    new file:   React/Filtering Animation/README.md 	modified:   React/Filtering Animation/filtering-animation/README.md 	modified:   React/Filtering Animation/filtering-animation/src/App.css 	modified:   React/Filtering Animation/filtering-animation/src/App.js 	modified:   React/Filtering Animation/filtering-animation/src/Filter.js 	modified:   React/Filtering Animation/filtering-animation/src/Movie.js 	modified:   React/Filtering Animation/filtering-animation/src/index.js

--- React/Filtering Animation/filtering-animation/src/App.js
+++ React/Filtering Animation/filtering-animation/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useEffect, useState} from 'react';
 import Movie from './Movie';
 import Filter from './Filter';
+import {motion, AnimatePresence} from "framer-motion";
 
 function App() {
 
@@ -22,12 +23,18 @@ const fetchPopularMovies = async () => {
 }
   return (
     <div className="App">
-      <div className="popular-movies">
-        <Filter popular={popular} setFiltered={setFiltered}/>
-        {popular.map((movie) => {
-          return <Movie key={movie.id} movie={movie}/>;
-        })}
-      </div>
+        <Filter 
+          popular={popular} 
+          setFiltered={setFiltered} 
+          activeGenre={activeGenre} 
+          setActiveGenre={setActiveGenre}/>
+        <motion.div layout className="popular-movies">
+          <AnimatePresence>
+            {filtered.map((movie) => {
+            return <Movie key={movie.id} movie={movie}/>;
+            })}
+          </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
--- React/Filtering Animation/filtering-animation/src/Filter.js
+++ React/Filtering Animation/filtering-animation/src/Filter.js
@@ -1,13 +1,26 @@
-import { useEffect } from "react";
+import {useEffect} from "react";
 
-function Filter(){
+function Filter({setActiveGenre, activeGenre, setFiltered, popular}){
+
+    useEffect(() => {
+        if (activeGenre === 0) {
+            setFiltered(popular);
+                return;
+        }
+
+    const filtered = popular.filter((movie) => movie.genre_ids.includes(activeGenre));
+    
+    setFiltered(filtered);
+
+}, [activeGenre]);
+
     return(
         <div className="filter-container">
-            <button>Todos</button>
-            <button>Comedia</button>
-            <button>Ação</button>
+            <button className={activeGenre === 0 ? "active" : ""} onClick={() => setActiveGenre(0)}>Todos</button>
+            <button className={activeGenre === 35 ? "active" : ""} onClick={() => setActiveGenre(35)}>Comedia</button>
+            <button className={activeGenre === 28 ? "active" : ""} onClick={() => setActiveGenre(28)}>Ação</button>
         </div>
-    )
+    );
 }
 
 export default Filter;
